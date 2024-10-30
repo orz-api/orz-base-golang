2729834f45db35efc01d68aea4f579a9199ac085
reset7523 <reset7523@gmail.com>
2024-10-30 18:12:10 +0800

    Dev: add props

--- props.go
+++ props.go
@@ -1,7 +1,7 @@
 package orzbase
 
 type Props struct {
-	Service string `json:"service" yaml:"service" toml:"service"`
+	Service string `json:"service,omitempty" yaml:"service,omitempty" toml:"service,omitempty"`
 }
 
 var PropsObj = Props{
